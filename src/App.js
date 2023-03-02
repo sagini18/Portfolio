c504f8fd18f7542f693a8ecd2ad684d8120ef8d1
sagini18 <saginisaju@gmail.com>
2023-03-02 10:46:29 +0530

    Qualification

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/services";
+import Qualification from "./components/qualification/Qualification";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <About />
         <Skills />
         <Services />
+        <Qualification />
+        {/* Testimonial */}
       </main>
     </div>
   );
